a95bf266fdae6da35f4c2a85bffdb3bbdf87e7c1
EvgenyiRus <42252829+EvgenyiRus@users.noreply.github.com>
2020-10-12 16:40:06 +0300

    Add main logical

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,5 +1,27 @@
-public class Calculator {
-    public static void main(String[] args) {
+import IO.Data;
+import IO.IData;
+import Service.IOperationService;
+import Service.OperationService;
+import Util.RomanNumberUtil;
 
+import java.io.IOException;
+
+public class Calculator {
+    private static boolean isRomanOperation = false;
+
+    public static void main(String[] args) throws IOException {
+        IData data = new Data();
+        String[] numericExpression = data.readData();
+        if (RomanNumberUtil.isRomanNumber(numericExpression[0]) && RomanNumberUtil.isRomanNumber(numericExpression[2])) {
+            isRomanOperation = true;
+            RomanNumberUtil.getRomanNumberValue(numericExpression);
+        }
+        IOperationService operationService = new OperationService(numericExpression);
+        double result = operationService.getResult();
+        if (isRomanOperation) {
+            data.printResult(RomanNumberUtil.arabicToRoman((int) result));
+        } else {
+            data.printResult(result);
+        }
     }
 }
